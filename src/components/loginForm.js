301dc5efc308d1cef571c399293d5fd218e8f6f0
neusc <neusc829@163.com>
2018-03-13 22:57:03 +0800

    使用firebase登陆认证逻辑的实现

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -4,6 +4,8 @@
  * Date: 2018/3/12
  */
 import React, { Component } from 'react'
+import { Text } from 'react-native'
+import firebase from 'firebase'
 import { Card, CardSection, Button, Input } from './common'
 
 class loginForm extends Component {
@@ -13,6 +15,20 @@ class loginForm extends Component {
       email: '',
       password: ''
     }
+  }
+
+  onButtonPress () {
+    // 必须在构造函数里绑定this或使用箭头函数来绑定当前作用域到this
+    const { email, password } = this.state
+    this.setState({ error: '' })
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => console.log('signIn'))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(() => console.log('signUp'))
+          .catch((e) => {
+            console.log(e)
+            this.setState({ error: 'Authentication Failed.' })
+          })
+      })
   }
 
   render () {
@@ -35,13 +51,22 @@ class loginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+        <Text style={styles.errorStyle}>{this.state.error}</Text>
         <CardSection>
-          <Button>
+          <Button onPress={() => this.onButtonPress()}>
             log in
           </Button>
         </CardSection>
       </Card>
     )
+  }
+}
+
+const styles = {
+  errorStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
